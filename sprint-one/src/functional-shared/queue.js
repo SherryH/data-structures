4142d772af52a0420588d483225a19cc8ffa09c9
SherryH <enjoy.wind7rain@gmail.com>
2016-12-14 16:28:40 +1300

    Completed the funtional-share style for queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+    count : 0
+  };
+  obj.enqueue = queueMethods.enqueue;
+  obj.dequeue = queueMethods.dequeue;
+  obj.size = queueMethods.size;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(input){
+    this.count++;
+    this[this.count] = input;
+  },
+  dequeue: function(){
+    var keys = Object.keys(this);
+    var numKeys = keys.filter(function(value){
+      return !isNaN(parseInt(value));
+    });
+    var smallest = Math.min(...numKeys);
+    var temp = this[smallest];
+    delete this[smallest];
+    return temp;
+  },
+  size: function(){
+    return Object.keys(this).length - 4;
+  },
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj ={
+    count : 0
+  };
+
+  obj.push = stackMethods.push;
+  obj.pop = stackMethods.pop;
+  obj.size = stackMethods.size;
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(input){
+    this[this.count+1] = input;
+    this.count++;
+  },
+  pop : function(){
+    var temp = this[this.count];
+    delete this[this.count];
+    if (this.count > 0){
+      this.count--;
+    }
+    return temp;
+  },
+  size : function(){
+    return this.count;
+  }
+};
 
 
