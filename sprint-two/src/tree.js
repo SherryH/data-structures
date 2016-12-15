5b3cc1b2402c0d51dd45444ef7b57e658498c9ad
SherryH <enjoy.wind7rain@gmail.com>
2016-12-15 16:19:27 +1300

    Completed the tree class

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,11 +13,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
-};
+  var answer = false;
 
+  var testChildren = function(input) {
+    if (input.value === target) {
+      answer = true;
+    } else if (input.children.length > 0) {
+      for (var i = 0; i < input.children.length; i++) {
+        testChildren(input.children[i]);
+      }
+    }
+  };
+
+  testChildren(this);
+  return answer;
+};
 
 
 /*
